3b782a8c7090303f78b20bfcbb9fa47537c8a0d0
jw1036 <jw1036@gmail.com>
2022-01-05 23:23:31 +0900

    2.6 Styles part Two

--- App.js
+++ App.js
@@ -9,20 +9,32 @@ export default function App() {
       <View style={styles.city}>
         <Text style={styles.cityName}>Seoul</Text>
       </View>
-      <View style={styles.weather}>
+      <ScrollView
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.descryption}>Sunny</Text>
         </View>
-      </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.descryption}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.descryption}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "tomato",
+    flex: 1, backgroundColor: "tomato",
   },
   city: {
     flex: 1.2,
@@ -34,10 +46,9 @@ const styles = StyleSheet.create({
     fontWeight: "500",
   },
   weather: {
-    flex: 3,
   },
   day: {
-    flex: 1,
+    width: SCREEN_WIDTH,
     alignItems: "center",
   },
   temp: {
