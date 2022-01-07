80a25260f389fd49f5f523e891c3542792e86753
jw1036 <jw1036@gmail.com>
2022-01-07 22:37:24 +0900

    3.2 TextInput

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import {
   StyleSheet,
   Text,
@@ -7,22 +8,43 @@ import {
   TouchableHighlight,
   TouchableWithoutFeedback,
   Pressable,
+  TextInput,
 } from 'react-native';
+import { color } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 import { theme } from './Colors';
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+
+  const work = () => setWorking(true);
+  const travel = () => setWorking(false);
+
+  const onChangeText = (payload) => setText(payload);
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
 
       <View style={styles.header}>
-        <TouchableOpacity onPress={() => console.log("pressed")}>
-          <Text style={styles.btnText}>Work</Text>
+        <TouchableOpacity onPress={work}>
+          <Text style={{ ...styles.btnText, color: working ? "white" : theme.gray }}>
+            Work
+          </Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => console.log("pressed")}>
-          <Text style={styles.btnText}>Travel</Text>
+        <TouchableOpacity onPress={travel}>
+          <Text style={{ ...styles.btnText, color: !working ? "white" : theme.gray }}>
+            Travel
+          </Text>
         </TouchableOpacity>
       </View>
+
+      <TextInput
+        onChangeText={onChangeText}
+        value={text}
+        placeholder={working ? "Add a To Do" : "Where do you want to go?"}
+        style={styles.input}
+      />
     </View>
   );
 }
@@ -42,5 +64,13 @@ const styles = StyleSheet.create({
     fontSize: 38,
     fontWeight: "600",
     color: "white",
-  }
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18,
+  },
 });
