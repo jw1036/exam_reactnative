24d072c5e67167122669c409bcb1d79d641e50fc
jw1036 <jw1036@gmail.com>
2022-01-23 21:32:33 +0900

    9.3 앱 아이콘과 로딩 화면

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import React from 'react';
-import { StatusBar } from 'react-native';
+import React, { useState } from 'react';
+import { Image, StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { theme } from './theme';
+import { Asset } from 'expo-asset';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
+const cacheImages = images => {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+};
+
+const cacheFonts = fonts => {
+  return fonts.map(font => Font.loadAsync(font));
+};
 
 const App = () => {
-  return (
+  const [isReady, setIsReady] = useState(false);
+
+  const _loadAssets = async () => {
+    const imageAssets = cacheImages([require('../assets/splash.png')]);
+    const fontAssets = cacheFonts([]);
+
+    await Promise.all([...imageAssets, ...fontAssets]);
+  };
+
+  return isReady ? (
     <ThemeProvider theme={theme}>
       <StatusBar barStyle="dark-content" />
     </ThemeProvider>
+  ) : (
+    <AppLoading
+      startAsync={_loadAssets}
+      onFinish={() => setIsReady(true)}
+      onError={console.warn}
+    />
   );
 };
 
