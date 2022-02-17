39954d783dcb2843e90ca8b83ed31ed08aa4e192
jw1036 <jw1036@gmail.com>
2022-02-17 23:09:45 +0900

    3.2 TextInput으로 사용자 키보드 입력받기

--- components/AddTodo.js
+++ components/AddTodo.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, TextInput, View} from 'react-native';
 
 function AddTodo() {
-  return <View style={styles.block} />;
+  return (
+    <View style={styles.block}>
+      <TextInput placeholder="할일을 입력하세요" style={styles.input} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   block: {
     height: 64,
-    backgroundColor: 'red',
+    paddingHorizontal: 16,
+    borderColor: '#bdbdbd',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    justifyContent: 'center',
+  },
+  input: {
+    fontSize: 16,
+    paddingVertical: 8,
   },
 });
 
