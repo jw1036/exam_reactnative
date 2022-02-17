1d62f8f6a16872da8b8fbd9eb45b0eed13d30299
jw1036 <jw1036@gmail.com>
2022-02-17 23:17:27 +0900

    3.2.2 useState로 텍스트 상태 값 관리하기

--- components/AddTodo.js
+++ components/AddTodo.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, TextInput, View} from 'react-native';
 
 function AddTodo() {
+  const [text, setText] = useState('');
+
   return (
     <View style={styles.block}>
-      <TextInput placeholder="할일을 입력하세요" style={styles.input} />
+      <TextInput
+        placeholder="할일을 입력하세요"
+        style={styles.input}
+        value={text}
+        onChangeText={setText}
+      />
     </View>
   );
 }
