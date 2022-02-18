fe2b791fd80e6b18816c9f41526ae435d5c3f3e2
jw1036 <jw1036@gmail.com>
2022-02-18 22:25:09 +0900

    4.2.5 완료한 항목에 다른 스타일 적용하기

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 
 function TodoItem({id, text, done}) {
   return (
     <View style={styles.item}>
-      <View style={styles.circle} />
-      <Text style={styles.text}>{text}</Text>
+      <View style={[styles.circle, done && styles.filled]}>
+        {done && (
+          <Image
+            source={require('../assets/icons/check_white/check_white.png')}
+          />
+        )}
+      </View>
+      <Text style={[styles.text, done && styles.lineThrough]}>{text}</Text>
     </View>
   );
 }
@@ -24,10 +30,19 @@ const styles = StyleSheet.create({
     borderColor: '#26a69a',
     marginRight: 16,
   },
+  filled: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#26a69a',
+  },
   text: {
     flex: 1,
     fontSize: 16,
     color: '#212121',
+  },
+  lineThrough: {
+    color: '#9e9e9e',
+    textDecorationLine: 'line-through',
   },
 });
 
