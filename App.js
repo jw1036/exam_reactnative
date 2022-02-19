09f77c3a72f05051599eda5b30221d03b9bae842
jw1036 <jw1036@gmail.com>
2022-02-19 18:46:46 +0900

    5.2.1 네이티브 스택 내비게이터
    $ yarn add @react-navigation/native-stack
    $ cd iOS && pod install

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
-import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import DetailScreen from './screens/DetailScreen';
+
+const Stack = createNativeStackNavigator();
 
 function App() {
-  return <NavigationContainer></NavigationContainer>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Button, View} from 'react-native';
+
+function HomeScreen() {
+  return (
+    <View>
+      <Button title="Detail 열기" />
+    </View>
+  );
+}
+
+export default HomeScreen;
