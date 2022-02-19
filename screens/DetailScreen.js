25702e83266ea1f61e5ac0bdcd1b8ec2cec9fe44
jw1036 <jw1036@gmail.com>
2022-02-19 19:06:33 +0900

    5.2.3 라우트 파라미터

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
-function DetailScreen() {
+function DetailScreen({route, navigation}) {
   return (
-    <View>
-      <Text>Detail</Text>
+    <View style={styles.block}>
+      <Text style={styles.text}>id: {route.params.id}</Text>
+      <Button
+        title="다음"
+        onPress={() => navigation.navigate('Detail', {id: route.params.id + 1})}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 48,
+  },
+});
 
 export default DetailScreen;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,8 +5,16 @@ function HomeScreen({navigation}) {
   return (
     <View>
       <Button
-        title="Detail 열기"
-        onPress={() => navigation.navigate('Detail')}
+        title="Detail 1 열기"
+        onPress={() => navigation.push('Detail', {id: 1})}
+      />
+      <Button
+        title="Detail 2 열기"
+        onPress={() => navigation.push('Detail', {id: 2})}
+      />
+      <Button
+        title="Detail 3 열기"
+        onPress={() => navigation.push('Detail', {id: 3})}
       />
     </View>
   );
