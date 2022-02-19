3aec0c74d45feeaea18b1ed7d152ffd0549599fa
jw1036 <jw1036@gmail.com>
2022-02-19 19:09:38 +0900

    5.2.4 뒤로가기

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -5,10 +5,14 @@ function DetailScreen({route, navigation}) {
   return (
     <View style={styles.block}>
       <Text style={styles.text}>id: {route.params.id}</Text>
-      <Button
-        title="다음"
-        onPress={() => navigation.navigate('Detail', {id: route.params.id + 1})}
-      />
+      <View>
+        <Button
+          title="다음"
+          onPress={() => navigation.push('Detail', {id: route.params.id + 1})}
+        />
+        <Button title="뒤로가기" onPress={() => navigation.pop()} />
+        <Button title="처음으로" onPress={() => navigation.popToTop()} />
+      </View>
     </View>
   );
 }
