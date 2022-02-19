83b4f733156f693562c55a1129f6a7700bb876c0
jw1036 <jw1036@gmail.com>
2022-02-20 00:05:58 +0900

    5.3.1 드로어 내비게이터
    $ yarn add @react-navigation/drawer
    $ yarn add react-native-gesture-handler
    $ yarn add react-native-reanimated@~2.2.0
    $ cd ios && pod install

---
+++ AppDrawer.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {View, Text, Button} from 'react-native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+const Drawer = createDrawerNavigator();
+
+function HomeScreen({navigation}) {
+  return (
+    <View>
+      <Text>Home</Text>
+      <Button title="Drawer 열기" onPress={() => navigation.openDrawer()} />
+      <Button
+        title="Setting 열기"
+        onPress={() => navigation.navigate('Setting')}
+      />
+    </View>
+  );
+}
+
+function SettingScreen({navigation}) {
+  return (
+    <View>
+      <Text>Setting</Text>
+      <Button title="뒤로가기" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        initialRouteName="Home"
+        drawerPosition="left"
+        backBehavior="history"
+        screenOptions={{
+          drawerActiveBackgroundColor: '#fb8c00',
+          drawerActiveTintColor: 'white',
+          headerShown: false,
+        }}
+        drawerContent={({navigation}) => (
+          <SafeAreaView>
+            <Text>A Custom Drawer</Text>
+            <Button
+              title="Drawer 닫기"
+              onPress={() => navigation.closeDrawer()}
+            />
+          </SafeAreaView>
+        )}>
+        <Drawer.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: '홈', headerLeft: () => <Text>Left</Text>}}
+        />
+        <Drawer.Screen
+          name="Setting"
+          component={SettingScreen}
+          options={{title: '설정'}}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
