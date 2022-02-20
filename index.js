f3fded8b903c1a2531e7dc6ec673cdb56dfc564d
jw1036 <jw1036@gmail.com>
2022-02-20 13:09:02 +0900

    5.3.4 머티리얼 하단 탭 내비게이터
    $ yarn add @react-navigation/material-bottom-tabs
    $ yarn add react-native-paper
    $ cd ios && pod install

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './AppMaterialTobTabs';
+import App from './AppMaterialBottomTabs';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
---
+++ screens/MainScreenMaterialBottomTabs.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {Text, View, Button} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const Tab = createMaterialBottomTabNavigator();
+
+function HomeScreen({navigation}) {
+  return (
+    <View>
+      <Text>Home</Text>
+      <Button
+        title="Detail 1 열기"
+        onPress={() => navigation.push('Detail', {id: 1})}
+      />
+    </View>
+  );
+}
+
+function SearchScreen() {
+  return (
+    <View>
+      <Text>Search</Text>
+    </View>
+  );
+}
+
+function NotificationScreen() {
+  return (
+    <View>
+      <Text>Notification</Text>
+    </View>
+  );
+}
+
+function MessageScreen() {
+  return (
+    <View>
+      <Text>Message</Text>
+    </View>
+  );
+}
+
+function MainScreen() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      tabBarOptions={{
+        showIcon: true,
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: '홈',
+          tabBarIcon: ({color}) => <Icon name="home" color={color} size={24} />,
+          tabBarColor: 'black',
+          tabBarBadge: 'new',
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{
+          tabBarLabel: '검색',
+          tabBarIcon: ({color}) => (
+            <Icon name="search" color={color} size={24} />
+          ),
+          tabBarColor: 'gray',
+        }}
+      />
+      <Tab.Screen
+        name="Notification"
+        component={NotificationScreen}
+        options={{
+          tabBarLabel: '알림',
+          tabBarIcon: ({color}) => (
+            <Icon name="notifications" color={color} size={24} />
+          ),
+          tabBarColor: 'green',
+          tabBarBadge: 30,
+        }}
+      />
+      <Tab.Screen
+        name="Message"
+        component={MessageScreen}
+        options={{
+          tabBarLabel: '메시지',
+          tabBarIcon: ({color}) => (
+            <Icon name="message" color={color} size={24} />
+          ),
+          tabBarColor: 'blue',
+          tabBarBadge: true,
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+export default MainScreen;
