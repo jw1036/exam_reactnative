00b6c145d8c12bfa915e9c27c432bac94a663772
jw1036 <jw1036@gmail.com>
2022-02-20 12:46:54 +0900

    5.3.3 머티리얼 상단 탭 내비게이션
    $ yarn add @react-navigation/material-top-tabs
    $ yarn add react-native-tab-view react-native-pager-view
    $ cd ios && pod install

---
+++ AppMaterialTobTabs.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import MainScreen from './screens/MainScreenMaterialTopTabs';
+import DetailScreen from './screens/DetailScreen';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Main">
+        <Stack.Screen name="Main" component={MainScreen} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+export default App;
---
+++ screens/MainScreenMaterialTopTabs.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import {Text, View, Button} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const Tab = createMaterialTopTabNavigator();
+
+function HomeScreen({navigation}) {
+  return (
+    <View>
+      <Text>Home</Text>
+      <Button
+        title="Detail 1 열기"
+        onPress={() => navigation.push('Detail', {id: 1})}
+      />
+    </View>
+  );
+}
+
+function SearchScreen() {
+  return (
+    <View>
+      <Text>Search</Text>
+    </View>
+  );
+}
+
+function NotificationScreen() {
+  return (
+    <View>
+      <Text>Notification</Text>
+    </View>
+  );
+}
+
+function MessageScreen() {
+  return (
+    <View>
+      <Text>Message</Text>
+    </View>
+  );
+}
+
+function MainScreen() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        tabBarIndicatorStyle: {
+          backgroundColor: '#009688',
+        },
+        tabBarActiveTintColor: '#009688',
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: '홈',
+          tabBarIcon: ({color}) => <Icon name="home" color={color} size={24} />,
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{
+          tabBarLabel: '검색',
+          tabBarIcon: ({color}) => (
+            <Icon name="search" color={color} size={24} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notification"
+        component={NotificationScreen}
+        options={{
+          tabBarLabel: '알림',
+          tabBarIcon: ({color}) => (
+            <Icon name="notifications" color={color} size={24} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Message"
+        component={MessageScreen}
+        options={{
+          tabBarLabel: '메시지',
+          tabBarIcon: ({color}) => (
+            <Icon name="message" color={color} size={24} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+export default MainScreen;
