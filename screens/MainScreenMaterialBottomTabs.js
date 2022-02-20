8f5e6fedb57a763eb75b7f93e88b4de03bcfa671
jw1036 <jw1036@gmail.com>
2022-02-20 13:22:43 +0900

    5.4.1 useNavigation

--- screens/MainScreenMaterialBottomTabs.js
+++ screens/MainScreenMaterialBottomTabs.js
@@ -2,17 +2,26 @@ import React from 'react';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import {Text, View, Button} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import {useNavigation} from '@react-navigation/native';
 
 const Tab = createMaterialBottomTabNavigator();
+
+function OpenDetailButton() {
+  const navigation = useNavigation();
+
+  return (
+    <Button
+      title="Detail 1 열기"
+      onPress={() => navigation.push('Detail', {id: 1})}
+    />
+  );
+}
 
 function HomeScreen({navigation}) {
   return (
     <View>
       <Text>Home</Text>
-      <Button
-        title="Detail 1 열기"
-        onPress={() => navigation.push('Detail', {id: 1})}
-      />
+      <OpenDetailButton />
     </View>
   );
 }
