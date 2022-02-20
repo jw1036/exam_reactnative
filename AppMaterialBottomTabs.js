380f276c0e95b56b845364307f356db8c5e8ba00
jw1036 <jw1036@gmail.com>
2022-02-20 13:17:47 +0900

    5.3.5 머티리얼 하단 탭 내비게이터 헤더 타이틀 동기화하기

--- AppMaterialBottomTabs.js
+++ AppMaterialBottomTabs.js
@@ -1,16 +1,35 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {
+  getFocusedRouteNameFromRoute,
+  NavigationContainer,
+} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MainScreen from './screens/MainScreenMaterialBottomTabs';
 import DetailScreen from './screens/DetailScreen';
 
 const Stack = createNativeStackNavigator();
 
+function getHeaderTitle(route) {
+  const routeName = getFocusedRouteNameFromRoute(route) ?? 'Home';
+  const nameMap = {
+    Home: '홈',
+    Search: '검색',
+    Notification: '알림',
+    Message: '메시지',
+  };
+
+  return nameMap[routeName];
+}
+
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Main">
-        <Stack.Screen name="Main" component={MainScreen} />
+        <Stack.Screen
+          name="Main"
+          component={MainScreen}
+          options={({route}) => ({title: getHeaderTitle(route)})}
+        />
         <Stack.Screen name="Detail" component={DetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
