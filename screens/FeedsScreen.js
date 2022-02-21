597c4f2e6eca06d1c225950287e7a63c0b59becc
jw1036 <jw1036@gmail.com>
2022-02-21 23:15:04 +0900

    6.4.2 FeedList 컴포넌트 만들기

--- screens/FeedsScreen.js
+++ screens/FeedsScreen.js
@@ -1,14 +1,15 @@
 import React, {useContext} from 'react';
 import {StyleSheet, View} from 'react-native';
+import FeedList from '../components/FeedList';
 import FloatWriteButton from '../components/FloatWriteButton';
 import LogContext from '../contexts/LogContext';
 
 function FeedsScreen() {
   const {logs} = useContext(LogContext);
-  console.log(JSON.stringify(logs, null, 2));
 
   return (
     <View style={styles.block}>
+      <FeedList logs={logs} />
       <FloatWriteButton />
     </View>
   );
