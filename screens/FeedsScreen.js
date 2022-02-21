4290750b231a14887a4cb1fc85e5fdf5cc0fbffb
jw1036 <jw1036@gmail.com>
2022-02-21 20:22:42 +0900

    6.2.2 useContext Hook 함수

--- screens/FeedsScreen.js
+++ screens/FeedsScreen.js
@@ -1,27 +1,19 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import LogContext from '../contexts/LogContext';
 
 function FeedsScreen() {
+  const value = useContext(LogContext);
+
   return (
     <View style={styles.block}>
-      <Box>{value => <Text>{value}</Text>}</Box>
+      <Text>{value}</Text>
     </View>
   );
 }
 
-function Box({children}) {
-  return <View style={styles.box}>{children('Hello World')}</View>;
-}
-
 const styles = StyleSheet.create({
   block: {},
-  box: {
-    borderWidth: 2,
-    padding: 16,
-    borderBottomColor: 'black',
-    marginBottom: 16,
-  },
 });
 
 export default FeedsScreen;
