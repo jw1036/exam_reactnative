9c295fae66e1396e0c53805fc32d2d6f42fb2adf
jw1036 <jw1036@gmail.com>
2022-02-21 22:11:46 +0900

    6.3.3 useRef로 컴포넌트 레퍼런스 선택하기

--- components/WriteEditor.js
+++ components/WriteEditor.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {StyleSheet, TextInput, View} from 'react-native';
 
 function WriteEditor({title, body, onChangeTitle, onChangeBody}) {
+  const bodyRef = useRef();
+
   return (
     <View style={styles.block}>
       <TextInput
@@ -10,6 +12,9 @@ function WriteEditor({title, body, onChangeTitle, onChangeBody}) {
         returnKeyType="next"
         onChange={onChangeTitle}
         value={title}
+        onSubmitEditing={() => {
+          bodyRef.current.focus();
+        }}
       />
       <TextInput
         placeholder="당신의 오늘을 기록해보세요"
@@ -18,6 +23,7 @@ function WriteEditor({title, body, onChangeTitle, onChangeBody}) {
         textAlignVertical="top"
         onChange={onChangeBody}
         value={body}
+        ref={bodyRef}
       />
     </View>
   );
