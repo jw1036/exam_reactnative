7ca2d2637c2a3e84e7039a77eb3d40161f178e60
jw1036 <jw1036@gmail.com>
2022-02-21 23:04:36 +0900

    6.4.1 FeedListItem 컴포넌트 만들기

---
+++ components/FeedListItem.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {Platform, Pressable, StyleSheet, Text} from 'react-native';
+
+function truncate(text) {
+  const replaced = text.replace(/\n/g, ' ');
+  if (replaced.length <= 100) {
+    return replaced;
+  }
+  return replaced.slice(0, 100).concat('...');
+}
+
+function FeedListItem({log}) {
+  const {title, body, date} = log;
+
+  return (
+    <Pressable
+      style={({pressed}) => [
+        styles.block,
+        Platform.OS === 'ios' && pressed && {backgroundColor: '#efefef'},
+      ]}
+      android_ripple={{color: '#ededed'}}>
+      <Text style={styles.date}>{new Date(date).toLocaleString()}</Text>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.body}>{truncate(body)}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  block: {
+    backgroundColor: 'white',
+    paddingHorizontal: 16,
+    paddingVertical: 24,
+  },
+  date: {
+    fontSize: 12,
+    color: '#546e7a',
+    marginBottom: 8,
+  },
+  title: {
+    color: '#263238',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  body: {
+    color: '#37474f',
+    fontSize: 16,
+    lineHeight: 21,
+  },
+});
+
+export default FeedListItem;
