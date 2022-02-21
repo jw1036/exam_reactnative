066a9dde2eb849da1e11455d6399b9c009eb9859
jw1036 <jw1036@gmail.com>
2022-02-21 22:18:44 +0900

    6.3.5 WriteScreen에서 텍스트 상태 관리하기

--- screens/WriteScreen.js
+++ screens/WriteScreen.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {KeyboardAvoidingView, Platform, StyleSheet} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import WriteEditor from '../components/WriteEditor';
 import WriteHeader from '../components/WriteHeader';
 
 function WriteScreen() {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
   return (
     <SafeAreaView style={styles.block}>
       <KeyboardAvoidingView
         style={styles.avoidingView}
         behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
         <WriteHeader />
-        <WriteEditor />
+        <WriteEditor
+          title={title}
+          body={body}
+          onChangeTitle={setTitle}
+          onChangeBody={setBody}
+        />
       </KeyboardAvoidingView>
     </SafeAreaView>
   );
