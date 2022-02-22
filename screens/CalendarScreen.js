ee3d1db4a7601c63970d4c95f238c2122bf72225
jw1036 <jw1036@gmail.com>
2022-02-22 20:57:06 +0900

    6.5.1 애니메이션 연습하기

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,6 +1,48 @@
-import React, {useContext} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import React, {useContext, useEffect, useRef, useState} from 'react';
+import {StyleSheet, View, Text, Animated, Button} from 'react-native';
 import LogContext from '../contexts/LogContext';
+
+function SlideLeftAndRight() {
+  const animation = useRef(new Animated.Value(0)).current;
+  const [enabled, setEnabled] = useState(false);
+
+  useEffect(() => {
+    Animated.timing(animation, {
+      toValue: enabled ? 1 : 0,
+      useNativeDriver: true,
+    }).start();
+  }, [enabled, animation]);
+
+  return (
+    <View>
+      <Animated.View
+        style={[
+          styles.rectangle,
+          {
+            transform: [
+              {
+                translateX: animation.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [0, 150],
+                }),
+              },
+            ],
+            opacity: animation.interpolate({
+              inputRange: [0, 1],
+              outputRange: [1, 0],
+            }),
+          },
+        ]}
+      />
+      <Button
+        title="Toggle"
+        onPress={() => {
+          setEnabled(!enabled);
+        }}
+      />
+    </View>
+  );
+}
 
 function CalendarScreen() {
   const {text} = useContext(LogContext);
@@ -8,6 +50,7 @@ function CalendarScreen() {
   return (
     <View style={styles.block}>
       <Text style={styles.text}>text: {text}</Text>
+      <SlideLeftAndRight />
     </View>
   );
 }
@@ -17,6 +60,11 @@ const styles = StyleSheet.create({
   text: {
     padding: 16,
     fontSize: 24,
+  },
+  rectangle: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'black',
   },
 });
 
