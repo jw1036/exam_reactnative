bf276eadc15f9b157239da37b5903b1f8f399c60
jw1036 <jw1036@gmail.com>
2022-02-23 22:17:05 +0900

    7.4.6 EmptySearchResult 만들기

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,5 +1,6 @@
 import React, {useContext} from 'react';
 import {StyleSheet, View} from 'react-native';
+import EmptySearchResult from '../components/EmptySearchResult';
 import FeedList from '../components/FeedList';
 import LogContext from '../contexts/LogContext';
 import SearchContext from '../contexts/SearchContext';
@@ -14,6 +15,14 @@ function SearchScreen() {
       : logs.filter(log =>
           [log.title, log.body].some(text => text.includes(keyword)),
         );
+
+  if (keyword === '') {
+    return <EmptySearchResult type="EMPTY_KEYWORD" />;
+  }
+
+  if (filtered.length === 0) {
+    return <EmptySearchResult type="NOT_FOUND" />;
+  }
 
   return (
     <View style={styles.block}>
