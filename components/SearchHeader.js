2516bfe5bad25e85f275c6497c1bb5c54c36240e
jw1036 <jw1036@gmail.com>
2022-02-23 21:42:43 +0900

    7.4.3 SearchHeader 컴포넌트 UI 구성하기

--- components/SearchHeader.js
+++ components/SearchHeader.js
@@ -1,16 +1,41 @@
 import React from 'react';
-import {StyleSheet, View, useWindowDimensions} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  useWindowDimensions,
+  TextInput,
+  Pressable,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 function SearchHeader() {
   const {width} = useWindowDimensions();
 
-  return <View style={[styles.block, {width: width - 32, height: 24}]} />;
+  return (
+    <View style={[styles.block, {width: width - 32}]}>
+      <TextInput
+        style={styles.input}
+        placeholder="검색어를 입력하세요"
+        autoFocus
+      />
+      <Pressable
+        style={({pressed}) => [styles.button, pressed && {opacity: 0.5}]}>
+        <Icon name="cancel" size={20} color="#9e9e9e" />
+      </Pressable>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   block: {
-    color: 'white',
-    backgroundColor: 'blue',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+  },
+  button: {
+    marginLeft: 8,
   },
 });
 
