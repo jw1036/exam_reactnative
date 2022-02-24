364ea968413c083f655485848719398dcab5a223
jw1036 <jw1036@gmail.com>
2022-02-24 20:57:32 +0900

    7.5.4 useMemo Hook으로 최적화하기

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,5 +1,5 @@
 import {format} from 'date-fns';
-import React, {useContext, useState} from 'react';
+import React, {useContext, useMemo, useState} from 'react';
 import CalendarView from '../components/CalendarView';
 import FeedList from '../components/FeedList';
 import LogContext from '../contexts/LogContext';
@@ -9,11 +9,15 @@ function CalendarScreen() {
   const [selectedDate, setSelectedDate] = useState(
     format(new Date(), 'yyyy-MM-dd'),
   );
-  const markedDates = logs.reduce((acc, cur) => {
-    const formatedDate = format(new Date(cur.date), 'yyyy-MM-dd');
-    acc[formatedDate] = {marked: true};
-    return acc;
-  }, {});
+  const markedDates = useMemo(
+    () =>
+      logs.reduce((acc, cur) => {
+        const formatedDate = format(new Date(cur.date), 'yyyy-MM-dd');
+        acc[formatedDate] = {marked: true};
+        return acc;
+      }, {}),
+    [logs],
+  );
   const filteredLog = logs.filter(
     log => format(new Date(log.date), 'yyyy-MM-dd') === selectedDate,
   );
