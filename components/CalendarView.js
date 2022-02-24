defa5e8999e57944887e283e57f269f9994f835e
jw1036 <jw1036@gmail.com>
2022-02-24 19:53:32 +0900

    7.5.1 달력에 표시하기

--- components/CalendarView.js
+++ components/CalendarView.js
@@ -3,7 +3,24 @@ import {StyleSheet} from 'react-native';
 import {Calendar} from 'react-native-calendars';
 
 function CalendarView() {
-  return <Calendar style={styles.block} />;
+  const markedDates = {
+    '2022-02-16': {selected: true},
+    '2022-02-17': {marked: true},
+    '2022-02-18': {marked: true},
+  };
+
+  return (
+    <Calendar
+      style={styles.block}
+      markedDates={markedDates}
+      theme={{
+        selectedDayBackgroundColor: '#009688',
+        arrowColor: '#009688',
+        dotColor: '#009688',
+        todayTextColor: '#009688',
+      }}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
