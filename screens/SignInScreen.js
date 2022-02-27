34c813b7f4be0ca24057e98c08b0b951c1a1ad96
jw1036 <jw1036@gmail.com>
2022-02-27 22:54:20 +0900

    8.2.2 인풋 상태 관리하기

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,46 +1,102 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import BorderedInput from '../components/BorderedInput';
 import CustomButton from '../components/CustomButton';
 
 function SignInScreen({navigation, route}) {
   const {isSignUp} = route.params ?? {};
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const createChangeTextHandler = name => value => {
+    setForm({...form, [name]: value});
+  };
+  const onSubmit = () => {
+    Keyboard.dismiss();
+    console.log(form);
+  };
 
   return (
-    <SafeAreaView style={styles.fullscreen}>
-      <Text style={styles.text}>PublicGallery</Text>
-      <View style={styles.form}>
-        <BorderedInput hasMarginBottom placeholder="이메일" />
-        <BorderedInput placeholder="비밀번호" hasMarginBottom={isSignUp} />
-        {isSignUp && <BorderedInput placeholder="비밀번호 확인" />}
-        <View style={styles.buttons}>
-          {isSignUp ? (
-            <>
-              <CustomButton title="회원인증" hasMarginBottom />
-              <CustomButton
-                onPress={() => navigation.goBack()}
-                title="로그인"
-                theme="secondary"
-              />
-            </>
-          ) : (
-            <>
-              <CustomButton title="로그인" hasMarginBottom />
-              <CustomButton
-                onPress={() => navigation.push('SignIn', {isSignUp: true})}
-                title="회원인증"
-                theme="secondary"
-              />
-            </>
+    <KeyboardAvoidingView
+      style={styles.keyboardAvoidingView}
+      behavior={Platform.select({ios: 'padding'})}>
+      <SafeAreaView style={styles.fullscreen}>
+        <Text style={styles.text}>PublicGallery</Text>
+        <View style={styles.form}>
+          <BorderedInput
+            hasMarginBottom
+            placeholder="이메일"
+            value={form.email}
+            onChangeText={createChangeTextHandler('email')}
+            autoCapitalize="none"
+            autoCorrect={false}
+            autoCompleteType="email"
+            keyboardType="email-address"
+          />
+          <BorderedInput
+            placeholder="비밀번호"
+            hasMarginBottom={isSignUp}
+            value={form.password}
+            onChangeText={createChangeTextHandler('password')}
+            secureTextEntry
+          />
+          {isSignUp && (
+            <BorderedInput
+              placeholder="비밀번호 확인"
+              value={form.confirmPassword}
+              onChangeText={createChangeTextHandler('confirmPassword')}
+              secureTextEntry
+            />
           )}
+          <View style={styles.buttons}>
+            {isSignUp ? (
+              <>
+                <CustomButton
+                  onPress={() => onSubmit()}
+                  title="회원인증"
+                  hasMarginBottom
+                />
+                <CustomButton
+                  onPress={() => navigation.goBack()}
+                  title="로그인"
+                  theme="secondary"
+                />
+              </>
+            ) : (
+              <>
+                <CustomButton
+                  onPress={() => onSubmit()}
+                  title="로그인"
+                  hasMarginBottom
+                />
+                <CustomButton
+                  onPress={() => navigation.push('SignIn', {isSignUp: true})}
+                  title="회원인증"
+                  theme="secondary"
+                />
+              </>
+            )}
+          </View>
         </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
+  keyboardAvoidingView: {
+    flex: 1,
+  },
   fullscreen: {
     flex: 1,
     alignItems: 'center',
