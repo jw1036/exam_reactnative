a21ecc25a4efb4e3e2fc6d93c7aebae76c4de4ab
jw1036 <jw1036@gmail.com>
2022-02-27 22:35:14 +0900

    8.2.1 회원 인증을 위한 UI 준비하기
    8.2.1.1 rest 연산자와 spread 연산자로 모든 Props 그대로 넘겨주기
    8.2.1.2 CustomButton에 Secondary 버튼 스타일 만들기
    8.2.1.3 라우트 파라미터로 회원가입 화면 만들기

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import BorderedInput from '../components/BorderedInput';
+import CustomButton from '../components/CustomButton';
 
-function SignInScreen() {
+function SignInScreen({navigation, route}) {
+  const {isSignUp} = route.params ?? {};
+
   return (
     <SafeAreaView style={styles.fullscreen}>
       <Text style={styles.text}>PublicGallery</Text>
+      <View style={styles.form}>
+        <BorderedInput hasMarginBottom placeholder="이메일" />
+        <BorderedInput placeholder="비밀번호" hasMarginBottom={isSignUp} />
+        {isSignUp && <BorderedInput placeholder="비밀번호 확인" />}
+        <View style={styles.buttons}>
+          {isSignUp ? (
+            <>
+              <CustomButton title="회원인증" hasMarginBottom />
+              <CustomButton
+                onPress={() => navigation.goBack()}
+                title="로그인"
+                theme="secondary"
+              />
+            </>
+          ) : (
+            <>
+              <CustomButton title="로그인" hasMarginBottom />
+              <CustomButton
+                onPress={() => navigation.push('SignIn', {isSignUp: true})}
+                title="회원인증"
+                theme="secondary"
+              />
+            </>
+          )}
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
@@ -19,6 +49,14 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 30,
     fontWeight: 'bold',
+  },
+  form: {
+    marginTop: 64,
+    width: '100%',
+    paddingHorizontal: 16,
+  },
+  buttons: {
+    marginTop: 64,
   },
 });
 
