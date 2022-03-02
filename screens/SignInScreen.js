8f3687e0664ae24be50d07d3331a3dc5ed0aac65
jw1036 <jw1036@gmail.com>
2022-03-02 23:35:55 +0900

    8.2.6 오류 예외 처리하기

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -29,14 +29,27 @@ function SignInScreen({navigation, route}) {
   const onSubmit = async () => {
     Keyboard.dismiss();
 
-    const {email, password} = form;
+    const {email, password, confirmPassword} = form;
+
+    if (isSignUp && password !== confirmPassword) {
+      Alert.alert('실패', '비밀번호가 일치하지 않습니다.');
+      return;
+    }
+
     const info = {email, password};
     setLoading(true);
     try {
       const {user} = isSignUp ? await signUp(info) : await signIn(info);
       console.log(user);
     } catch (e) {
-      Alert.alert('실패');
+      const message = {
+        'auth/email-already-in-use': '이미 가입된 이메일입니다.',
+        'auth/wrong-password': '잘못된 비밀번호입니다.',
+        'auth/user-not-found': '존재하지 않는 계정입니다.',
+        'auth/invalid-email': '유효하지 않은 이메일 주소입니다.',
+      };
+      const msg = message[e.code] || `${isSignUp ? '가입' : '로그인'} 실패`;
+      Alert.alert('실패', msg);
       console.log(e);
     } finally {
       setLoading(false);
