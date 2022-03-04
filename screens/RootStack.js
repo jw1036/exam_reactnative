b2e82716353488ecc4e886b00e6d746b9a75d3a1
jw1036 <jw1036@gmail.com>
2022-03-04 22:54:54 +0900

    9.2 로그인 상태 유지하기

--- screens/RootStack.js
+++ screens/RootStack.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SignInScreen from './SignInScreen';
 import WelcomeScreen from './WelcomeScreen';
 import {useUserContext} from '../contexts/UserContext';
 import MainTab from './MainTab';
+import {subscribeAuth} from '../lib/auth';
+import {getUser} from '../lib/user';
 
 const Stack = createNativeStackNavigator();
 
 function RootStack() {
-  const {user} = useUserContext();
+  const {user, setUser} = useUserContext();
+
+  useEffect(() => {
+    const unscribe = subscribeAuth(async currentUser => {
+      unscribe();
+      if (!currentUser) {
+        return;
+      }
+      const profile = await getUser(currentUser.uid);
+      if (!profile) {
+        return;
+      }
+      setUser(profile);
+    });
+  }, [setUser]);
 
   return (
     <Stack.Navigator>
