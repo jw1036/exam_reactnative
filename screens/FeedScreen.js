41b7761da29e815965c8fadebd154644f57bd945
jw1036 <jw1036@gmail.com>
2022-03-06 21:37:46 +0900

    9.4.4.2 포스트 더 불러우기

--- screens/FeedScreen.js
+++ screens/FeedScreen.js
@@ -1,20 +1,41 @@
 import React, {useEffect, useState} from 'react';
-import {FlatList} from 'react-native';
+import {FlatList, ActivityIndicator, StyleSheet} from 'react-native';
 import PostCard from '../components/PostCard';
-import {getPosts} from '../lib/posts';
+import {getPosts, getOlderPosts, PAGE_SIZE} from '../lib/posts';
 
 function FeedScreen() {
   const [posts, setPosts] = useState(null);
+  const [noMorePost, setNoMorePost] = useState(false);
 
   useEffect(() => {
     getPosts().then(setPosts);
   }, []);
+
+  const onLoadMore = async () => {
+    if (noMorePost || !posts || posts.length < PAGE_SIZE) {
+      return;
+    }
+    const lastPost = posts[posts.length - 1];
+    const olderPosts = await getOlderPosts(lastPost.id);
+    if (olderPosts.length < PAGE_SIZE) {
+      setNoMorePost(true);
+    }
+    setPosts(posts.concat(olderPosts));
+  };
 
   return (
     <FlatList
       data={posts}
       renderItem={renderItem}
       keyExtractor={item => item.id}
+      contentContainerStyle={styles.container}
+      onEndReached={onLoadMore}
+      onEndReachedThreshold={0.75}
+      ListFooterComponent={
+        !noMorePost && (
+          <ActivityIndicator style={styles.spinner} size={32} color="#6200ee" />
+        )
+      }
     />
   );
 }
@@ -28,5 +49,14 @@ const renderItem = ({item}) => (
     photoURL={item.photoURL}
   />
 );
+
+const styles = StyleSheet.create({
+  container: {
+    paddingBottom: 48,
+  },
+  spinner: {
+    height: 64,
+  },
+});
 
 export default FeedScreen;
