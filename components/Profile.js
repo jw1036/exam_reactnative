579958797eb30c383f9bac4bb1896519ff103c3d
jw1036 <jw1036@gmail.com>
2022-03-06 23:18:02 +0900

    9.5.7 커스텀 Hook을 작성해 컴포넌트 리팩토링하기

--- components/Profile.js
+++ components/Profile.js
@@ -4,51 +4,21 @@ import {
   FlatList,
   StyleSheet,
   View,
-  Image,
   Text,
   RefreshControl,
 } from 'react-native';
-import {getNewerPosts, getOlderPosts, getPosts, PAGE_SIZE} from '../lib/posts';
 import {getUser} from '../lib/user';
 import Avatar from './Avatar';
 import PostGridItem from './PostGridItem';
+import usePosts from '../hooks/usePosts';
 
 function Profile({userId}) {
   const [user, setUser] = useState(null);
-  const [posts, setPosts] = useState(null);
-
-  const [noMorePost, setNoMorePost] = useState(false);
-  const [refreshing, setRefreshing] = useState(false);
-
-  const onLoadMore = async () => {
-    if (noMorePost || !posts || posts.length < PAGE_SIZE) {
-      return;
-    }
-    const lastPost = posts[posts.length - 1];
-    const olderPosts = await getOlderPosts(lastPost.id, userId);
-    if (olderPosts.length < PAGE_SIZE) {
-      setNoMorePost(true);
-    }
-    setPosts(posts.concat(olderPosts));
-  };
-
-  const onRefresh = async () => {
-    if (!posts || posts.length === 0 || refreshing) {
-      return;
-    }
-    const firstPost = posts[0];
-    setRefreshing(true);
-    const newerPosts = await getNewerPosts(firstPost.id, userId);
-    setRefreshing(false);
-    if (newerPosts.length === 0) {
-      return;
-    }
-    setPosts(newerPosts.concat(posts));
-  };
+  const {posts, noMorePost, refreshing, onLoadMore, onRefresh} =
+    usePosts(userId);
 
   useEffect(() => {
     getUser(userId).then(setUser);
-    getPosts(userId).then(setPosts);
   }, [userId]);
 
   if (!user || !posts) {
--- screens/FeedScreen.js
+++ screens/FeedScreen.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {
   FlatList,
   ActivityIndicator,
@@ -6,42 +6,10 @@ import {
   RefreshControl,
 } from 'react-native';
 import PostCard from '../components/PostCard';
-import {getPosts, getOlderPosts, PAGE_SIZE, getNewerPosts} from '../lib/posts';
+import usePosts from '../hooks/usePosts';
 
 function FeedScreen() {
-  const [posts, setPosts] = useState(null);
-  const [noMorePost, setNoMorePost] = useState(false);
-  const [refreshing, setRefreshing] = useState(false);
-
-  useEffect(() => {
-    getPosts().then(setPosts);
-  }, []);
-
-  const onLoadMore = async () => {
-    if (noMorePost || !posts || posts.length < PAGE_SIZE) {
-      return;
-    }
-    const lastPost = posts[posts.length - 1];
-    const olderPosts = await getOlderPosts(lastPost.id);
-    if (olderPosts.length < PAGE_SIZE) {
-      setNoMorePost(true);
-    }
-    setPosts(posts.concat(olderPosts));
-  };
-
-  const onRefresh = async () => {
-    if (!posts || posts.length === 0 || refreshing) {
-      return;
-    }
-    const firstPost = posts[0];
-    setRefreshing(true);
-    const newerPosts = await getNewerPosts(firstPost.id);
-    setRefreshing(false);
-    if (newerPosts.length === 0) {
-      return;
-    }
-    setPosts(newerPosts.concat(posts));
-  };
+  const {posts, noMorePost, refreshing, onLoadMore, onRefresh} = usePosts();
 
   return (
     <FlatList
