2e0435abd5ab2f5de076b3435c4fcd7a17801d46
jw1036 <jw1036@gmail.com>
2022-03-07 21:43:38 +0900

    9.6.1 재사용할 수 있는 모달 만들기

--- components/CameraButton.js
+++ components/CameraButton.js
@@ -9,8 +9,8 @@ import {
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
-import UploadModeModal from './UploadModeModal';
 import {useNavigation} from '@react-navigation/native';
+import ActionSheetModal from './ActionSheetModal';
 
 const TABBAR_HEIGHT = 49;
 
@@ -80,11 +80,21 @@ function CameraButton() {
           <Icon name="camera-alt" color="white" size={24} />
         </Pressable>
       </View>
-      <UploadModeModal
+      <ActionSheetModal
         visible={modalVisible}
         onClose={() => setModalVisible(false)}
-        onLaunchCamera={onLaunchCamera}
-        onLaunchImageLibrary={onLaunchImageLibrary}
+        actions={[
+          {
+            icon: 'camera-alt',
+            text: '카메라로 촬영하기',
+            onPress: onLaunchCamera,
+          },
+          {
+            icon: 'photo',
+            text: '사진 선택하기',
+            onPress: onLaunchImageLibrary,
+          },
+        ]}
       />
     </>
   );
