0579c0407b4c16c1f9c7cfa3408a4412ac98610b
jw1036 <jw1036@gmail.com>
2022-03-10 00:10:31 +0900

    13.5.3 posts 모듈 만들기

--- slices/index.ts
+++ slices/index.ts
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import auth from './auth';
 import todos from './todos';
+import posts from './posts';
 
 const rootReducer = combineReducers({
   auth,
   todos,
+  posts,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
