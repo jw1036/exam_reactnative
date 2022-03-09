1684c8e1211ad62936084e59c885a5694548cf26
jw1036 <jw1036@gmail.com>
2022-03-09 23:51:50 +0900

    13.5.1 미들웨어 적용하기

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
 import rootReducer from './slices';
 import AuthApp from './components/AuthApp';
 import TodoApp from './components/TodoApp';
+import {configureStore} from '@reduxjs/toolkit';
 
-const store = createStore(rootReducer);
+const store = configureStore({reducer: rootReducer});
 
 function App() {
   return (
