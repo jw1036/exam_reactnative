f7aea2625043763dd52f4bd0abcbd661a91692cd
jw1036 <jw1036@gmail.com>
2022-03-09 23:19:17 +0900

    13.4.1 todos 모듈 작성하기

---
+++ slices/todos.ts
@@ -0,0 +1,45 @@
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+
+export interface Todo {
+  id: number;
+  text: string;
+  done: boolean;
+}
+
+const initialState: Todo[] = [
+  {id: 1, text: '리액트 네이티브 배우기', done: true},
+  {id: 2, text: '상태 관리 배우기', done: false},
+];
+
+let nextId = 3;
+
+const todosSlice = createSlice({
+  name: 'todos',
+  initialState,
+  reducers: {
+    add: {
+      prepare(text: string) {
+        const prepared = {payload: {id: nextId, text}};
+        nextId += 1;
+        return prepared;
+      },
+      reducer(state, action: PayloadAction<{id: number; text: string}>) {
+        state.push({...action.payload, done: false});
+      },
+    },
+    remove(state, action: PayloadAction<number>) {
+      const index = state.findIndex(todo => todo.id === action.payload);
+      state.splice(index);
+    },
+    toggle(state, action: PayloadAction<number>) {
+      const selected = state.find(todo => todo.id === action.payload);
+      if (!selected) {
+        return;
+      }
+      selected.done = !selected.done;
+    },
+  },
+});
+
+export const {add, remove, toggle} = todosSlice.actions;
+export default todosSlice.reducer;
