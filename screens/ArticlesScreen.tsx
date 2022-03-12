919bca15556c787c28a21923dd86e3c6ebb1414e
jw1036 <jw1036@gmail.com>
2022-03-12 23:43:13 +0900

    15.4.2 useQuery 사용하기

--- screens/ArticlesScreen.tsx
+++ screens/ArticlesScreen.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {useQuery} from 'react-query';
+import {getArticles} from '../api/articles';
 
 function ArticlesScreen() {
+  const {data, isLoading} = useQuery('articles', getArticles);
+
+  console.log({data, isLoading});
+
   return (
     <View>
       <Text>Articles</Text>
