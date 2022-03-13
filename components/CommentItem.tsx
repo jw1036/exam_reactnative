98666b1d303b3d361db448ae827c23675e55d9f2
jw1036 <jw1036@gmail.com>
2022-03-13 19:11:18 +0900

    15.5.4 컴포넌트 만들기

---
+++ components/CommentItem.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export interface CommentItemProps {
+  id: number;
+  message: string;
+  username: string;
+  publishedAt: string;
+}
+
+function CommentItem({message, username, publishedAt}: CommentItemProps) {
+  const formattedDate = new Date(publishedAt).toLocaleString();
+  return (
+    <View style={styles.block}>
+      <View style={styles.head}>
+        <Text style={styles.username}>{username}</Text>
+        <Text style={styles.date}>{formattedDate}</Text>
+      </View>
+      <Text style={styles.message}>{message}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  block: {
+    paddingTop: 8,
+    paddingBottom: 16,
+  },
+  head: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  username: {
+    fontWeight: 'bold',
+  },
+  date: {
+    color: '#546e7a',
+    fontSize: 10,
+    marginTop: 4,
+  },
+  message: {
+    marginTop: 4,
+  },
+});
+
+export default CommentItem;
--- screens/ArticleScreen.tsx
+++ screens/ArticleScreen.tsx
@@ -1,10 +1,13 @@
 import {RouteProp, useRoute} from '@react-navigation/native';
 import React from 'react';
-import {StyleSheet, View, Text, ActivityIndicator} from 'react-native';
+import {StyleSheet, ActivityIndicator, FlatList} from 'react-native';
 import {RootStackParamList} from './types';
 import {useQuery} from 'react-query';
 import {getArticle} from '../api/articles';
 import {getComments} from '../api/comments';
+import ArticleView from '../components/ArticleView';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import CommentItem from '../components/CommentItem';
 
 type ArticleScreenRouteProp = RouteProp<RootStackParamList, 'Article'>;
 
@@ -15,24 +18,53 @@ function ArticleScreen() {
   const articleQuery = useQuery(['article', id], () => getArticle(id));
   const commentsQuery = useQuery(['comments', id], () => getComments(id));
 
+  const {bottom} = useSafeAreaInsets();
+
   if (!articleQuery.data || !commentsQuery.data) {
     return (
       <ActivityIndicator size="large" style={styles.spinner} color="black" />
     );
   }
 
+  const {title, body, published_at, user} = articleQuery.data;
+
   return (
-    <View style={styles.block}>
-      <Text>{articleQuery.data.title}</Text>
-      <Text>{articleQuery.data.body}</Text>
-      <Text>{commentsQuery.data.length}개의 댓글</Text>
-    </View>
+    <FlatList
+      style={styles.flatList}
+      contentContainerStyle={[styles.flatListContent, {paddingBottom: bottom}]}
+      data={commentsQuery.data}
+      renderItem={({item}) => (
+        <CommentItem
+          id={item.id}
+          message={item.message}
+          publishedAt={item.published_at}
+          username={item.user.username}
+        />
+      )}
+      keyExtractor={item => item.id.toString()}
+      ListHeaderComponent={
+        <ArticleView
+          title={title}
+          body={body}
+          publishedAt={published_at}
+          username={user.username}
+        />
+      }
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  block: {},
-  spinner: {},
+  spinner: {
+    flex: 1,
+  },
+  flatList: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  flatListContent: {
+    paddingHorizontal: 12,
+  },
 });
 
 export default ArticleScreen;
