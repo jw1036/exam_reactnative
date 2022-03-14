5d8e5d2f8b65f4cfa0b682936e27fe46dab2cc87
jw1036 <jw1036@gmail.com>
2022-03-14 23:38:40 +0900

    15.10.2 useInfiniteQuery 사용하기

--- components/Articles.tsx
+++ components/Articles.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {FlatList, StyleSheet, View} from 'react-native';
+import {ActivityIndicator, FlatList, StyleSheet, View} from 'react-native';
 import {Article} from '../api/types';
 import ArticleItem from './ArticleItem';
 import WriteButton from './WriteButton';
@@ -7,9 +7,16 @@ import WriteButton from './WriteButton';
 export interface ArticlesProps {
   articles: Article[];
   showWriteButton?: boolean;
+  isFetchingNextPage: boolean;
+  fetchNextPage(): void;
 }
 
-function Articles({articles, showWriteButton}: ArticlesProps) {
+function Articles({
+  articles,
+  showWriteButton,
+  isFetchingNextPage,
+  fetchNextPage,
+}: ArticlesProps) {
   return (
     <FlatList
       data={articles}
@@ -25,9 +32,20 @@ function Articles({articles, showWriteButton}: ArticlesProps) {
       style={styles.list}
       ItemSeparatorComponent={() => <View style={styles.separator} />}
       ListHeaderComponent={() => (showWriteButton ? <WriteButton /> : null)}
-      ListFooterComponent={() =>
-        articles.length > 0 ? <View style={styles.separator} /> : null
-      }
+      ListFooterComponent={() => (
+        <>
+          {articles.length > 0 ? <View style={styles.separator} /> : null}
+          {isFetchingNextPage && (
+            <ActivityIndicator
+              size="small"
+              color="black"
+              style={styles.spinner}
+            />
+          )}
+        </>
+      )}
+      onEndReachedThreshold={0.5}
+      onEndReached={fetchNextPage}
     />
   );
 }
@@ -40,6 +58,11 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 1,
     backgroundColor: '#cfd8dc',
+  },
+  spinner: {
+    backgroundColor: 'white',
+    paddingTop: 32,
+    paddingBottom: 32,
   },
 });
 
--- screens/ArticlesScreen.tsx
+++ screens/ArticlesScreen.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import {ActivityIndicator, StyleSheet} from 'react-native';
-import {useQuery} from 'react-query';
+import {useInfiniteQuery} from 'react-query';
 import {getArticles} from '../api/articles';
+import {Article} from '../api/types';
 import Articles from '../components/Articles';
 import {useUserState} from '../contexts/UserContext';
 
 function ArticlesScreen() {
-  const {data} = useQuery('articles', getArticles);
+  // const {data} = useQuery('articles', getArticles);
+  const {data, isFetchingNextPage, fetchNextPage} = useInfiniteQuery(
+    'articles',
+    ({pageParam}) => getArticles({cursor: pageParam}),
+    {
+      getNextPageParam: lastPage =>
+        lastPage.length === 10 ? lastPage[lastPage.length - 1].id : undefined,
+    },
+  );
+  const items = useMemo(() => {
+    if (!data) {
+      return null;
+    }
+    return ([] as Article[]).concat(...data.pages);
+  }, [data]);
   const [user] = useUserState();
 
-  if (!data) {
+  if (!items) {
     return <ActivityIndicator size="large" style={styles.spinner} />;
   }
 
-  return <Articles articles={data} showWriteButton={!!user} />;
+  return (
+    <Articles
+      articles={items}
+      showWriteButton={!!user}
+      isFetchingNextPage={isFetchingNextPage}
+      fetchNextPage={fetchNextPage}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
