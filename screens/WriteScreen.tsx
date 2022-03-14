8cc8afd3765a124c36acb0b5e84cfd0cea74025d
jw1036 <jw1036@gmail.com>
2022-03-14 22:46:55 +0900

    15.9.5 useMutation으로 게시글 작성 API 호출하기

--- screens/WriteScreen.tsx
+++ screens/WriteScreen.tsx
@@ -9,16 +9,23 @@ import {
 import {SafeAreaView, useSafeAreaInsets} from 'react-native-safe-area-context';
 import {useNavigation} from '@react-navigation/native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import {useMutation} from 'react-query';
+import {writeArticle} from '../api/articles';
 
 function WriteScreen() {
   const {top} = useSafeAreaInsets();
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
+  const {mutate: write} = useMutation(writeArticle, {
+    onSuccess: () => {
+      navigation.goBack();
+    },
+  });
 
   const navigation = useNavigation();
   const onSubmit = useCallback(() => {
-    // TODO: 구현 예정
-  }, []);
+    write({title, body});
+  }, [body, title, write]);
 
   useEffect(() => {
     navigation.setOptions({
