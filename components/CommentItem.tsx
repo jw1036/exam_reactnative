93883c231c0e7accd748e400b651fe4edaf15061
jw1036 <jw1036@gmail.com>
2022-03-16 00:02:52 +0900

    15.12.3 댓글 삭제 기능 구현하기

--- components/CommentItem.tsx
+++ components/CommentItem.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
 
 export interface CommentItemProps {
   id: number;
   message: string;
   username: string;
   publishedAt: string;
+  isMyComment: boolean;
+  onRemove(id: number): void;
+  onModify(id: number): void;
 }
 
-function CommentItem({message, username, publishedAt}: CommentItemProps) {
+function CommentItem({
+  id,
+  message,
+  username,
+  publishedAt,
+  isMyComment,
+  onRemove,
+  onModify,
+}: CommentItemProps) {
   const formattedDate = new Date(publishedAt).toLocaleString();
+
+  const handleRemove = () => onRemove(id);
+  const handleModify = () => onModify(id);
+
   return (
     <View style={styles.block}>
       <View style={styles.head}>
@@ -17,6 +32,23 @@ function CommentItem({message, username, publishedAt}: CommentItemProps) {
         <Text style={styles.date}>{formattedDate}</Text>
       </View>
       <Text style={styles.message}>{message}</Text>
+      {isMyComment && (
+        <View style={styles.actionButtons}>
+          <Pressable
+            style={({pressed}) => pressed && styles.pressed}
+            hitSlop={8}
+            onPress={handleModify}>
+            <Text style={styles.buttonText}>수정</Text>
+          </Pressable>
+          <View style={styles.separator} />
+          <Pressable
+            style={({pressed}) => pressed && styles.pressed}
+            hitSlop={8}
+            onPress={handleRemove}>
+            <Text style={styles.buttonText}>삭제</Text>
+          </Pressable>
+        </View>
+      )}
     </View>
   );
 }
@@ -40,6 +72,21 @@ const styles = StyleSheet.create({
   },
   message: {
     marginTop: 4,
+  },
+  actionButtons: {
+    marginTop: 24,
+    justifyContent: 'flex-end',
+    flexDirection: 'row',
+  },
+  separator: {
+    width: 8,
+  },
+  buttonText: {
+    fontSize: 12,
+    color: '#546e7a',
+  },
+  pressed: {
+    opacity: 0.75,
   },
 });
 
